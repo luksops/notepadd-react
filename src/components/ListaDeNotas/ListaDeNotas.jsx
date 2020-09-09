11e9f49b31ce20cbf5dc13319144f9c3f6d2be2c
Lucas Boullosa <lucaskmboulhosa@gmail.com>
2020-09-09 09:22:15 +0100

    First commit

---
+++ src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import CardNota from '../CardNota';
+import './estilo.css';
+class ListaDeNotas extends Component {
+	render() {
+		return (
+			<ul className='lista-notas'>
+				{this.props.notas.map((objeto, index) => {
+					return (
+						<li className='lista-notas_item' key={index}>
+							<CardNota textoNota={objeto.texto} tituloNota={objeto.titulo} />
+						</li>
+					);
+				})}
+			</ul>
+		);
+	}
+}
+
+export default ListaDeNotas;
